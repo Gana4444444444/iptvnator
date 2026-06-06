c4eabdb8fcd051bb75feac9c049908cc923bc521
Gana4444444444 <samantamelo@prof.educacao.sp.gov.br>
2026-06-05 22:15:05 -0300

    Update environment.prod.ts

--- apps/web/src/environments/environment.prod.ts
+++ apps/web/src/environments/environment.prod.ts
@@ -4,5 +4,4 @@ export const AppConfig = {
     production: true,
     environment: 'PROD',
     version: packageJson.version,
-    BACKEND_URL: 'https://iptvnator-playlist-parser-api.vercel.app',
-};
+    BACKEND_URL: 'https://iptvnator-backend-azure.vercel.app',
